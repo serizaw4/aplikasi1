5065acd65632dee69595425823d62140c4df99d2
mubar3 <khusnul.mubar@yahoo.com>
2023-08-19 14:53:16 +0700

    pull

--- resources/views/pesanan.blade.php
+++ resources/views/pesanan.blade.php
@@ -73,26 +73,9 @@
     </div>
   </header><!-- End #header -->
 
-  <!-- ======= Hero Section ======= -->
-  <!-- <section id="hero">
-    <div class="hero-container">
-      <h1><b>Ayam Geprek</b></h1>
-      <h2><b>Ayam geprek nomor 1 Surabaya</b></h2> -->
-      <!-- <a href="#about" class="btn-get-started scrollto">Get Started</a> -->
-    <!-- </div>
-</section> -->
-
   <main id="main">
-
-    
-
-    <!-- ======= Our Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
       <div class="container">
-
-        <!-- <div class="section-title">
-          <h3><b>Our Menu</b></h3>
-        </div> -->
 
         <div class="row">
           <div class="col-lg-12">
@@ -101,173 +84,57 @@
 
               @if ($errors->has('message_success'))
   
-  <div class="p-4 bg-success border border-success-subtle rounded-3">
-  
-
-    <strong>{{ $errors->first('message_success') }}</strong>
-
-  </div>
-  @endif
-
-  @if ($errors->has('message'))
-  
-  <div class="p-4 bg-danger border border-danger-subtle rounded-3">
-  
-
-    <strong>{{ $errors->first('message') }}</strong>
-
-  </div>
-  @endif
-
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
-    <div class="col">
-      
-    </div>
-  </div>
-</div>  
-              
-            </ul>
-          </div>
-        </div>
-        
-              
-
-      </div>
-    </section><!-- End Our Portfolio Section -->
-
-    
-
-    <!-- ======= Contact Us Section ======= -->
-    <!-- <section id="contact" class="contact section-bg">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Contact Us</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="contact-about">
-              <h3>Amoeba</h3>
-              <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-              <div class="social-links">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              <div class="p-4 bg-success border border-success-subtle rounded-3">
+                <strong>{{ $errors->first('message_success') }}</strong>
               </div>
-            </div>
-          </div>
+              @endif
 
-          <div class="col-lg-3 col-md-6">
-            <div class="info">
-              <div>
-                <i class="bi bi-geo-alt"></i>
-                <p>A108 Adam Street<br>New York, NY 535022</p>
-              </div>
-
-              <div>
-                <i class="bi bi-envelope"></i>
-                <p>info@example.com</p>
-              </div>
-
-              <div>
-                <i class="bi bi-phone"></i>
-                <p>+1 5589 55488 55s</p>
-              </div>
-
-            </div>
-          </div> -->
-
-          <!-- <div class="col-lg-5 col-md-12">
-            @if ($errors->has('message_success'))
-  
-              <div class="p-4 bg-success border border-success-subtle rounded-3" style="color:white">
-            
-                  <strong>{{ $errors->first('message_success') }}</strong>
-                
-            
-              </div>
-            @endif
-          
-            @if ($errors->has('message'))
-          
+              @if ($errors->has('message'))
               <div class="p-4 bg-danger border border-danger-subtle rounded-3">
-            
-                  <strong>{{ $errors->first('message') }}</strong>
-                
-            
+                <strong>{{ $errors->first('message') }}</strong>
               </div>
-            @endif
+              @endif
 
-            <br> -->
-
-            <!-- <form action="{{ url('/kirim_pesan') }}" method="post" role="form">
-              @csrf
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-              </div>
-              <div class="form-group mt-3">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <br>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div> -->
-
-        </div>
-
+              <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First</th>
+                  <th scope="col">Last</th>
+                  <th scope="col">Handle</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+        </ul>
       </div>
-    </section><!-- End Contact Us Section -->
-
-    <!-- ======= Map Section ======= -->
-    <!-- <section class="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.2219901290355!2d-74.00369368400567!3d40.71312937933185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a23e28c1191%3A0x49f75d3281df052a!2s150%20Park%20Row%2C%20New%20York%2C%20NY%2010007%2C%20USA!5e0!3m2!1sen!2sbg!4v1579767901424!5m2!1sen!2sbg" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-    </section> -->
+    </section>
 
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="container">
+    <div class="container"  >
       
     </div>
   </footer><!-- End #footer -->
